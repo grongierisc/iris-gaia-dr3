960be52ec5e4f3c9773f510829e5951ac9a6d939
grongierisc <guillaume.rongier@intersystems.com>
2026-07-16 14:17:09 +0200

    Update production script and requirements for iris-pex-embedded-python version

--- init.db/10-start-production.sh
+++ init.db/10-start-production.sh
@@ -6,8 +6,7 @@ PYTHONPATH=/irisdev/app iris-persistence apply \
   gaia.models:SourceFluxAggregate \
   gaia.models:PhotometryChange \
   --backup-dir /tmp/iris/app/persistence-backup \
-  --to gaia-dr3-persistence-v1 \
-  --yes
+  --to gaia-dr3-persistence-v1 
 iop --migrate settings.py
 iop --default GaiaDR3.Production
 iop --start GaiaDR3.Production --detach
